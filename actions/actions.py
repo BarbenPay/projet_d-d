31fec6aeefbea8aa61e90ac1759810d74719bb25
BarbenPay <jean-baptiste.salanie@univ-tlse3.fr>
2026-01-13 08:50:06 +0100

    change with domain + new llm action

--- actions/actions.py
+++ actions/actions.py
@@ -12,6 +12,28 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 
+try:
+    from llama_cpp import Llama
+    print("Librairie llama-cpp-python chargée avec succès.")
+except ImportError:
+    print("Erreur: llama-cpp-python n'est pas installé.")
+    Llama = None
+    
+MODEL_PATH = "./models/Meta-Llama-3.1-8B-Instruct-Q3_K_M.gguf"
+
+llm = None
+if Llama:
+    try:
+        llm = Llama(
+            model_path=MODEL_PATH,
+            n_ctx=2048,
+            n_threads=4,
+            verbose=False
+        )
+        print(f"Modèle chargé depuis {MODEL_PATH}")
+    except Exception as e:
+        print(f"Impossible de charger le modèle : {e}")
+
 dictWeaponPossibilityDependingClass = {
             "paladin": ["longsword", "hammer", "shield", "mace", "sword"],
             "barbarian": ["axe", "longsword", "hammer", "mace"],
@@ -20,12 +42,51 @@ dictWeaponPossibilityDependingClass = {
             "ranger": ["bow", "dagger", "axe", "longsword"],
             "monk": ["staff", "dagger"],
 
-            "wizard": ["staff", "orb", "dagger"],
+            "wizard": ["staff", "orb"],
             "sorcerer": ["orb", "staff", "dagger"],
             "druid": ["staff", "mace"],
 
             "bard": ["luth", "dagger", "sword", "bow"]
                            }
+
+dictClassAbilities = {
+    "paladin": {
+        "name": "Divine Guardian",
+        "desc": "Grants +1 AC to adjacent allies when holding a Shield."
+    },
+    "barbarian": {
+        "name": "Feral Instinct",
+        "desc": "Deals +2 damage when HP is below 50%."
+    },
+    "rogue": {
+        "name": "Cheap Shot",
+        "desc": "First attack of combat deals bonus damage."
+    },
+    "ranger": {
+        "name": "Hunter's Mark",
+        "desc": "Consecutive attacks on the same target deal +2 damage."
+    },
+    "monk": {
+        "name": "Flow of Ki",
+        "desc": "Successful attacks increase Dodge chance by 10%."
+    },
+    "wizard": {
+        "name": "Arcane Study",
+        "desc": "Identifies enemy weaknesses using an Orb."
+    },
+    "sorcerer": {
+        "name": "Unstable Power",
+        "desc": "Re-roll damage results of 1 on spells."
+    },
+    "druid": {
+        "name": "Nature's Touch",
+        "desc": "Passive health regeneration of 2 HP per turn."
+    },
+    "bard": {
+        "name": "Inspiring Tune",
+        "desc": "Allies gain +1 Attack when the Bard holds a Luth."
+    }
+}
 
 dictSubraceDependingRace = {
     "elf" : ["high", "wood"],
@@ -135,37 +196,133 @@ class ActionAskSubrace(Action):
             dispatcher.utter_message(text="Cannot determine your current race.")
             return []
 
-        # Récupération de la liste
         subraces_list = dictSubraceDependingRace.get(player_race.lower(), [])
         
         print(f"DEBUG: Liste trouvée pour '{player_race}' = {subraces_list}")
 
-        # --- CHANGEMENT ICI : On prépare du TEXTE et des BOUTONS (plus de carousel) ---
-        
-        # 1. On prépare le message d'intro
+
         message_text = f"As a {player_race}, choose your legacy:\n\n"
+        
+        
+        for subrace_key in subraces_list:
+
+            description = dictNaturalAbilityFromSubrace.get(subrace_key, "Unknown ability")
+            display_title = subrace_key.capitalize()
+
+            message_text += f"🔹 **{display_title}**: {description}\n"
+
+        return []
+    
+class ActionAskClassWithAbility(Action):
+    def name(self) -> Text:
+        return "action_ask_race_with_ability"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+
+        message_text = f"Choose a class (paladin, rogue, wizard, druid, monk, barbarian, bard, ranger, sorcerer).\n\n"
         
         buttons_list = []
         
-        for subrace_key in subraces_list:
-            # On récupère la description
-            description = dictNaturalAbilityFromSubrace.get(subrace_key, "Unknown ability")
-            display_title = subrace_key.replace("_gnome", "").replace("_drow", "").capitalize()
+        for class_string in dictClassAbilities:
 
-            # 2. On ajoute la description dans le TEXTE principal (pour que le joueur la lise)
-            message_text += f"🔹 **{display_title}**: {description}\n"
+            description = class_string.get("desc", "Unknown description")
+            display_title = class_string.get("name", "Unknown name").capitalize()
 
-            # 3. On crée un bouton simple
+            message_text += f"🔹 **{display_title}**: {description}\n\n"
+
+
             buttons_list.append({
                 "title": f"Select {display_title}", 
-                "payload": f'/subrace{{"subrace": "{subrace_key}"}}'
+                "payload": f'/race{{"race": "{class_string}"}}'
             })
 
-        if not buttons_list:
-            dispatcher.utter_message(text=f"No subrace available for {player_race}.")
-            return []
 
-        # 4. On envoie le tout : Texte détaillé + Boutons en bas
-        dispatcher.utter_message(text=message_text, buttons=buttons_list)
+        dispatcher.utter_message(
+            text=message_text, 
+            buttons=buttons_list
+            )
+
+        return []
+    
+class askLLM(Action):
+    def name(self) -> Text:
+        return "action_ask_LLM"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        player_race = tracker.get_slot("race")
+        player_subrace = tracker.get_slot("subrace")
+        player_class = tracker.get_slot("class")
+        player_weapon = tracker.get_slot("weapon")
+        player_attributes = tracker.get_slot("attributes")
+        player_abilities = tracker.get_slot("abilities")
+        
+        current_message = tracker.latest_message.get('text')
+        
+        
+        if not llm:
+            dispatcher.utter_message(text="Désolé, le modèle d'IA n'est pas disponible pour le moment.")
+        
+        character_context = ""
+        if player_race & player_subrace:
+            character_context += "Le joueur incarne un {player_race}, sa sous-race est {player_subrace}. Cela lui apporte la capacité {dictNaturalAbilityFromSubrace.get(player_subrace)}"
+        if player_class & player_weapon:
+            character_context += "Le joueur a choisis la classe {player_class}. Il a également choisis comme arme principal un(e) {player_weapon}. Cela lui apporte la capacité de base {player_abilities}."
+        
+        system_prompt = (
+            "Tu es un Maître de donjon expert en Dongeon & Dragon. "
+            "Répond de manière immersive, brève et direct."
+            f"{character_context}"
+        )
+        
+        conversation_history = ""
+        
+        relevant_events = [e for e in tracker.events if e['event'] in ['user', 'bot']]
+        recent_events = relevant_events[-10:]
+        
+        if relevant_events & relevant_events[-1].get('text') == current_message:
+            events_to_process = relevant_events[:-1][-10:]
+        else:
+            events_to_process = relevant_events[-10:]
+        
+        for event in events_to_process:
+            if event['event'] == 'user' and event.get('text'):
+                conversation_history += f"<|start_header_id|>user<|end_header_id|>\n\n{event.get('text')}<|eot_id|>"
+            
+            elif event['event'] == 'bot' and event.get('text'):
+                conversation_history += f"<|start_header_id|>assistant<|end_header_id|>\n\n{event.get('text')}<|eot_id|>"
+                
+        
+        prompt = f"""<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+                    {system_prompt}
+                    <|eot_id|>
+                    
+                    {conversation_history}
+                    
+                    <|start_header_id|>user<|end_header_id|>
+                    {current_message}
+                    <|start_header_id|>assistant<|end_header_id|>
+                    """
+                    
+        if 'llm' not in globals() or llm is None:
+             dispatcher.utter_message(text="Le LLM est indisponible.")
+             return []
+         
+        
+        output = llm(
+            prompt,
+            max_tokens=350,
+            stop=["<|eot_id|>", "user:"],
+            echo=False,
+            temperature=0.7 
+        )
+        
+        response_text = output['choices'][0]['text'].strip()
+        dispatcher.utter_message(text=response_text)
 
         return []
